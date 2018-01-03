dcf5a86211dc13acacaf769d0aa79ef2f1023fde
NewfieRocket <newfierocket0@gmail.com>
2018-01-03 14:56:41 -0700

    update to tv.py for morer features

--- resources/tv.py
+++ resources/tv.py
@@ -1,7 +1,7 @@
 import gets
 import api
 
-class TvInfo(object):
+class TvInfo(object):   ####Get Meta Data info by search
     def __init__(self, tv_title, tv_year):
         self.name = tv_title   ###Sets Tv name name as string
         self.year = tv_year   ###Sets Year as String
@@ -16,10 +16,32 @@ class TvInfo(object):
         self.imdb = gets.GetImdb.imdb(self.original_title, self.year) 
         self.id = gets.GetId.id(self.meta)
 
-class TvSeasons(TvInfo):
+
+class TvSeasons(TvInfo):   ####Gets number of season for parsing episodes
     def __init__(self, tv_title, tv_year):
         super(TvSeasons, self).__init__(tv_title, tv_year)
-        self.season_query_string = '{base_url}tv/{tv_id}/season/{season_number}?api_key={api_key}'.format(base_url=self.base_url, tv_id=self.id, season_number=1, api_key=self.api_key)
-        # self.season_info = TvSeasons.metas(self.season_query_string)
-        self.meta_dict = gets.GetRequest.request(self.season_query_string)
+        self.season_query_string = '{base_url}tv/{tv_id}?api_key={api_key}'.format(base_url=self.base_url, tv_id=self.id, api_key=self.api_key)
+        self.season_meta_raw = gets.GetRequest.request(self.season_query_string)
+        self.num_of_seasons = self.season_meta_raw.get('number_of_seasons')
+
+
+class TvEpisodes(TvSeasons):   ####Will return a dict of all data for all seasons and episodes
+    def __init__(self, tv_title, tv_year):
+        super(TvEpisodes, self).__init__(tv_title, tv_year)
+        self.full_tv_dict = {}
+        for i in range(self.num_of_seasons):
+            self.episode_query_string = '{base_url}tv/{tv_id}/season/{season_number}?api_key={api_key}'.format(base_url=self.base_url, tv_id=self.id, season_number=(i+1), api_key=self.api_key)
+            # self.season_info = TvSeasons.metas(self.season_query_string)
+            self.episode_meta_raw = gets.GetRequest.request(self.episode_query_string)
+            self.full_tv_dict['season_' + str(i + 1)] = self.episode_meta_raw
         
+    def episode_info_list(self): ####Returns a dict of all seasons with all episode names and poster art.
+        self.episode_dict = {}
+        # self.episode_dict['poster_path'] = (self.season_meta_raw.get('poster_path'))
+        for s in range(len(self.full_tv_dict)):
+            self.episode_dict['season_' + str(s+1)] = {}
+            self.episode_dict['season_' + str(s+1)]['poster_path'] = self.image_base_url + self.full_tv_dict.get('season_' + str(s+1)).get('poster_path')
+            for i in range(len(self.full_tv_dict.get('season_' + str(s+1)).get('episodes'))):
+                self.episode_dict['season_' + str(s + 1)]['episode_' + str(i+1)] = self.full_tv_dict.get('season_'+ str(s+1)).get('episodes')[i].get('name')
+
+
